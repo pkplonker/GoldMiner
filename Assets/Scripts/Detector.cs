86a82f98231835b6c656d736afd865b012253981
stuart <pkplonker@gmail.com>
2022-07-16 17:50:29 +0100

    v3

--- Assets/Scripts/Detector.cs
+++ Assets/Scripts/Detector.cs
@@ -13,21 +13,18 @@ public class Detector : MonoBehaviour
 	[SerializeField] private Animator animator;
 	[SerializeField] private Rig rig;
 	private float directionTimer;
-	[SerializeField] private float directionTime = 2f;
-	private bool movingLeft;
-	[SerializeField] private Transform leftPos;
-	[SerializeField] private Transform rightPos;
-	[SerializeField] private float rotDegrees = 80f;
-	private Vector3 leftRot;
-	private Vector3 rightRot;
-
+	[SerializeField] private float targetRotAmount = 45;
+	private bool movingLeft = true;
+	[SerializeField] private float rotSpeed = 1f;
+	private float rotAmount;
+	private float leftRotTarget;
+	private float rightRotTarget;
 
 	private void Start()
 	{
-		leftRot = transform.localEulerAngles - new Vector3(0, rotDegrees / 2, 0);
-		rightRot = transform.localEulerAngles + new Vector3(0, rotDegrees / 2, 0);
+		leftRotTarget = 360 - targetRotAmount;
+		rightRotTarget = targetRotAmount;
 	}
-
 
 	private void Update()
 	{
@@ -37,25 +34,29 @@ public class Detector : MonoBehaviour
 
 	private void MoveDetector()
 	{
-		//determine if moving left or right
-		directionTimer += Time.deltaTime;
-		if (directionTimer >= directionTime)
-		{
-			movingLeft = !movingLeft;
-			directionTimer = 0;
-		}
-
-		var interpolationRatio = directionTimer / directionTime;
-		Debug.Log(interpolationRatio);
-		if (movingLeft)
-		{
-			transform.position = Vector3.Lerp(rightPos.position, leftPos.position, interpolationRatio);
-			transform.localEulerAngles = Vector3.Lerp(rightRot, leftRot, interpolationRatio);
+		if (!detecting) return;
+		var y = transform.localEulerAngles.y;
+		if (movingLeft && y < leftRotTarget && y > rightRotTarget)
+		{ 
+				movingLeft = false;
+			
 		}
 		else
 		{
-			transform.position = Vector3.Lerp(leftPos.position, rightPos.position, interpolationRatio);
-			transform.localEulerAngles = Vector3.Lerp(leftRot, rightRot, interpolationRatio);
+			if (y > rightRotTarget && y < leftRotTarget)
+			{
+				//move left
+				movingLeft = true;
+			}
+		}
+
+		if (movingLeft)
+		{
+			transform.Rotate(Vector3.up, -(rotSpeed * Time.deltaTime), Space.Self);
+		}
+		else
+		{
+			transform.Rotate(Vector3.up, (rotSpeed * Time.deltaTime), Space.Self);
 		}
 	}
 
