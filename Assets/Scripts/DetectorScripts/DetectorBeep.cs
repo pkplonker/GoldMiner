9fa516f574002c262a9e68b8b81f66aa9267d19a
stuart <pkplonker@gmail.com>
2022-07-19 11:22:37 +0100

    Signal using tone generator

--- Assets/Scripts/DetectorScripts/DetectorBeep.cs
+++ Assets/Scripts/DetectorScripts/DetectorBeep.cs
@@ -2,6 +2,7 @@
 // Copyright (C) 2022 Stuart Heath. All rights reserved.
 //
 
+using Audio;
 using UnityEngine;
 
 namespace DetectorScripts
@@ -12,43 +13,37 @@ namespace DetectorScripts
 	[RequireComponent(typeof(AudioSource))]
 	public class DetectorBeep : MonoBehaviour
 	{
-		private float beepTimer = 0;
-		[SerializeField]private float currentFrequency = 1f;
-
-		[SerializeField]private float lowestFrequency = 1f;
-		[SerializeField]private float highestFrequency = 0.3f;
-
 		AudioSource audioSource;
-		[SerializeField] private AudioClip beepClip;
-		[SerializeField] private float defaultPitch=1;
-		[SerializeField] private float maxPitch=1.3f;
-		[Range(0,1f)][SerializeField] private float currentSignalTest = 0f;
+		ToneGenerator.ToneClipData toneClipData;
+		[SerializeField] private float lowFreq;
+		[SerializeField] private float highFreq;
+
 		private void Start()
 		{
 			audioSource = GetComponent<AudioSource>();
+			toneClipData = new ToneGenerator.ToneClipData(0);
 		}
 
 		private void Update()
 		{
-			 //test
-			 //var signal = currentSignalTest;
-			 
-			 //production
-			 var signal = DetectorHead.currentSignal;
-			 
-			 
-			if (!DetectorState.isDetecting) return;
-			beepTimer += Time.deltaTime;
-			audioSource.pitch = Mathf.Lerp(defaultPitch, maxPitch,signal /*DetectorHead.currentSignal*/);
-			currentFrequency = Mathf.Lerp(lowestFrequency, highestFrequency,signal /*DetectorHead.currentSignal*/);
-
-			if (beepTimer >= currentFrequency)
+			var signal = DetectorHead.currentSignal;
+			if (signal < 0.1f)
 			{
-				beepTimer = 0;
-				if(signal> 0.02f)
-					audioSource.PlayOneShot(beepClip);
+				audioSource.Stop();
+				return;
 			}
 
+			//determine frequency
+			var freq = lowFreq + (highFreq - lowFreq) * signal;
+			//generate tone
+			var clipData = new ToneGenerator.ToneClipData(freq);
+			if (toneClipData == null || toneClipData.frequency == clipData.frequency) return;
+
+			toneClipData = clipData;
+			//play tone
+			audioSource.Stop();
+			audioSource.clip = clipData.clip;
+			audioSource.Play();
 		}
 	}
 }
