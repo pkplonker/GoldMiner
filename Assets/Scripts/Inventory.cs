fa90bdccbb0fb4f4f8f03e93a6915334e9469212
stuart <pkplonker@gmail.com>
2022-08-24 16:57:01 +0100

    Basic inventory started

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -6,16 +6,95 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-	private List<InventoryItem> _inventoryItems = new List<InventoryItem>();
-	public static Action OnInventoryChanged;
-	public bool Add(Item item)
-	{
-		return true;
-	}
-}
+	private List<InventorySlot> _slots = new List<InventorySlot>();
+	[SerializeField] private int _capacity=10;
+	public static event Action OnInventoryChanged;
 
-public class InventoryItem
-{
-	public Item _item;
-	public int amount;
+	private void Start()
+	{
+		FillSlotCapacity();
+	}
+
+	private void FillSlotCapacity()
+	{
+		_slots ??= new List<InventorySlot>();
+		if (_slots.Capacity > _capacity) _slots.Capacity = _capacity;
+		else if (_slots.Capacity < _capacity) _slots.Capacity = _capacity;
+		
+
+		while (_slots.Count < _slots.Capacity)
+		{
+			_slots.Add(new InventorySlot(null, 0));
+		}
+	}
+
+	public bool Add(Item item, int quantity = 1)
+	{
+		if (item == null) return false;
+		if (_slots.Any(t => t._item == item))
+		{
+			var slot = _slots.First(t => t._item == item);
+			slot.Add(item,quantity);
+			OnInventoryChanged?.Invoke();
+			return true;
+		}
+
+		//check for empty space
+		if (_slots.Any(t => t._item == null))
+		{
+			var slot = _slots.FirstOrDefault(t => t._item == null);
+			slot.Add(item, quantity);
+			OnInventoryChanged?.Invoke();
+			return true;
+		}
+
+		return false;
+	}
+
+	public bool Remove(Item item, int quantity = 1)
+	{
+		if (_slots.Any(t => t._item == item))
+		{
+			var slot = _slots.First(t => t._item == item);
+			slot.Remove(quantity);
+			OnInventoryChanged?.Invoke();
+			return true;
+		}
+		return false;
+	}
+
+	public bool RemoveAtSlot(int quantity, int index) => _slots[index].Remove(quantity);
+	[Serializable]
+	private class InventorySlot
+	{
+		public Item _item;
+		public int _quantity;
+
+		public InventorySlot(Item item, int quantity)
+		{
+			_item = item;
+			_quantity = quantity;
+		}
+		public void Add(Item item, int quantity)
+		{
+			_item = item;
+			_quantity += quantity;
+		}
+
+		public bool Remove(int quantity)
+		{
+			_quantity -= quantity;
+			switch (quantity)
+			{
+				case > 0:
+					return true;
+				case 0:
+					_item = null;
+					return true;
+				default:
+					_quantity += quantity;
+					return false;
+			}
+		}
+	}
 }
