5be9e71d2514fc23ce69b1788d00d003b831a946
stuart <pkplonker@gmail.com>
2022-08-24 14:51:38 +0100

    New currency system

---
+++ Assets/Scripts/Player/PlayerReference.cs
@@ -0,0 +1,14 @@
+ //
+ // Copyright (C) 2022 Stuart Heath. All rights reserved.
+ //
+using UnityEngine;
+
+    /// <summary>
+    ///PlayerReference full description
+    /// </summary>
+    	[CreateAssetMenu(fileName = "filename",menuName = "menuname")]
+
+public class PlayerReference : ScriptableObject
+{
+  
+}
---
+++ Assets/Scripts/PlayerCurrency.cs
@@ -0,0 +1,18 @@
+ //
+ // Copyright (C) 2022 Stuart Heath. All rights reserved.
+ //
+using UnityEngine;
+
+    /// <summary>
+    ///PlayerCurrency full description
+    /// </summary>
+    
+public class PlayerCurrency : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+    }
+}
