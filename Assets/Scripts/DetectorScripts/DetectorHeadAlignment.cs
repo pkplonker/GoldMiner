05db9ec28965e071adcb8e506a9d488ed6db1142
stuart <pkplonker@gmail.com>
2023-11-04 22:22:13 +0000

    improved detector head following terrain

--- Assets/Scripts/DetectorScripts/DetectorHeadAlignment.cs
+++ Assets/Scripts/DetectorScripts/DetectorHeadAlignment.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace DetectorScripts
 {
+	[RequireComponent(typeof(MeshFilter))]
 	public class DetectorHeadAlignment : MonoBehaviour
 	{
 		[SerializeField] private string groundLayer;
@@ -11,20 +12,36 @@ namespace DetectorScripts
 		[SerializeField] private GameObject shaft;
 		[SerializeField] private GameObject handleTarget;
 		[SerializeField] private float distanceBehindHandle = 0.2f;
+		private float radius;
+
+		private void Awake()
+		{
+			var bounds = GetComponent<MeshFilter>().mesh.bounds;
+
+			radius = Mathf.Max(bounds.max.x, bounds.max.z);
+			Debug.Log(radius);
+		}
+
+		RaycastHit[] results = new RaycastHit[10];
 
 		private void Update()
 		{
-			 
-			var results = Physics.RaycastAll(transform.position + new Vector3(0, 2f, 0), Vector3.down, 4f);
-
-			foreach (var result in results)
+			var ray = new Ray(transform.position + new Vector3(0, 2f, 0), Vector3.down);
+			var size = Physics.SphereCastNonAlloc(ray, radius, results);
+			for (int i = 0; i < size; i++)
 			{
-				if (result.collider.gameObject.layer != LayerMask.NameToLayer(groundLayer)) continue;
-				transform.position = new Vector3(transform.position.x, result.point.y + targetGroundOffset, transform.position.z);
-				Vector3 adjustedHandlePosition = handleTarget.transform.position - (handleTarget.transform.position - transform.position).normalized * distanceBehindHandle;
+				if (results[i].collider.gameObject.layer != LayerMask.NameToLayer(groundLayer)) continue;
+				var targetHeight = results[i].point.y;
+				Debug.Log(targetHeight);
+				transform.position = new Vector3(transform.position.x, targetHeight + targetGroundOffset,
+					transform.position.z);
+				Vector3 adjustedHandlePosition = handleTarget.transform.position -
+				                                 (handleTarget.transform.position - transform.position).normalized *
+				                                 distanceBehindHandle;
 				shaft.transform.position = (adjustedHandlePosition + transform.position) / 2f;
 				float distance = Vector3.Distance(adjustedHandlePosition, transform.position);
-				shaft.transform.localScale = new Vector3(shaft.transform.localScale.x, distance, shaft.transform.localScale.z);
+				shaft.transform.localScale =
+					new Vector3(shaft.transform.localScale.x, distance, shaft.transform.localScale.z);
 				shaft.transform.up = adjustedHandlePosition - transform.position;
 			}
 		}
