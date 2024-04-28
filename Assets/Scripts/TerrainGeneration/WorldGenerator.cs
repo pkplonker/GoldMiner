e0c3c6436c5f8b5a2d64fbfd169b74d812267e8f
PkplonkerGIT <pkplonker@gmail.com>
2024-04-28 15:49:30 +0100

    Tidyup

---
+++ Assets/Scripts/TerrainGeneration/WorldGenerator.cs
@@ -0,0 +1,18 @@
+ //
+ // Copyright (C) 2024 Stuart Heath. All rights reserved.
+ //
+using UnityEngine;
+
+    /// <summary>
+    ///WorldGenerator full description
+    /// </summary>
+    
+public class WorldGenerator : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+    }
+}
